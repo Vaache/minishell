50a0610e6078ad5c7fe01439bb480fe9f42c7c4c
Vachagan Hovhannisyan <vhovhann@c2r3s6.42yerevan.am>
2023-09-05 17:38:54 +0400

    norminette

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: vhovhann <vhovhann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 15:50:39 by vhovhann          #+#    #+#             */
-/*   Updated: 2023/09/04 20:22:21 by vhovhann         ###   ########.fr       */
+/*   Updated: 2023/09/05 16:32:41 by vhovhann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 void	minishell_echo(char **arr);
 
+int		echoik(char **arr, int i);
+void	echoyy(int i, char **arr);
+
 void	minishell_echo(char **arr)
 {
-	int	j;
 	int	i;
 
 	i = 1;
@@ -24,27 +26,9 @@ void	minishell_echo(char **arr)
 		ft_printf(1, "\n");
 	if (arr[i] && ft_strncmp(arr[i], "-n", 2) == 0)
 	{
-		while (arr[i] != NULL && ft_strncmp(arr[i], "-n", 2) == 0)
-		{
-			j = 1;
-			while (arr[i][j] && arr[i][j] == 'n')
-				j++;
-			if (arr[i][j] != '\0')
-				break ;
-			else if (arr[i + 1] && ft_strncmp(arr[i + 1], "-n", 2) != 0)
-			{
-				while (arr[i + 1])
-				{
-					ft_printf(1, "%s", arr[i + 1]);
-					if (arr[i + 2])
-						ft_printf(1, " ");
-					i++;
-					if (arr[i + 1] == NULL)
-						return ;
-				}
-			}
-			i++;
-		}
+		i = echoik(arr, i);
+		if (i == 0)
+			return ;
 		while (arr[i] && i != 1)
 		{
 			ft_printf(1, "%s", arr[i]);
@@ -56,15 +40,46 @@ void	minishell_echo(char **arr)
 		}
 	}
 	if (arr[i] && (ft_strncmp(arr[i], "-n", 2) != 0 || i == 1))
+		echoyy(i, arr);
+}
+
+int	echoik(char **arr, int i)
+{
+	int	j;
+
+	while (arr[i] != NULL && ft_strncmp(arr[i], "-n", 2) == 0)
 	{
-		while (arr[i])
+		j = 1;
+		while (arr[i][j] && arr[i][j] == 'n')
+			j++;
+		if (arr[i][j] != '\0')
+			break ;
+		else if (arr[i + 1] && ft_strncmp(arr[i + 1], "-n", 2) != 0)
 		{
-			ft_printf(1, "%s", arr[i]);
-			if (arr[i + 1])
-				ft_printf(1, " ");
-			i++;
-			if (arr[i] == NULL)
-				ft_printf(1, "\n");
+			while (arr[i + 1])
+			{
+				ft_printf(1, "%s", arr[i + 1]);
+				if (arr[i + 2])
+					ft_printf(1, " ");
+				i++;
+				if (arr[i + 1] == NULL)
+					return (0);
+			}
 		}
-	}	
+		i++;
+	}
+	return (i);
+}
+
+void	echoyy(int i, char **arr)
+{
+	while (arr[i])
+	{
+		ft_printf(1, "%s", arr[i]);
+		if (arr[i + 1])
+			ft_printf(1, " ");
+		i++;
+		if (arr[i] == NULL)
+			ft_printf(1, "\n");
+	}
 }
--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: vhovhann <vhovhann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 12:24:35 by vhovhann          #+#    #+#             */
-/*   Updated: 2023/09/05 16:06:56 by vhovhann         ###   ########.fr       */
+/*   Updated: 2023/09/05 17:26:11 by vhovhann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,35 @@
 void	minishell_exit(char **arr, t_env **env, char *s);
 int		exit_error_code(t_env *env);
 int		check_exit_status(char **arr, char *s, int exit_num);
+char	*check_zeroes(char *str);
 
 void	minishell_exit(char **arr, t_env **env, char *s)
 {
 	long long	exit_num;
 	char		*str;
 
-	if (arr[1] != NULL && (arr[1][0] == '0' || arr[1][0] == '+' || arr[1][1] == '0'))
+	if (arr[1] != NULL)
 	{
-		str = arr[1];		
-		arr[1] = trim_zeroes(arr[1]);
+		str = arr[1];
+		arr[1] = check_zeroes(arr[1]);
 		free(str);
 	}
 	exit_num = ft_atll(arr[1]);
 	s = ft_itul(exit_num);
-	if (arr[1] && arr[1][0] == '+')
+	if (arr[1] && arr[1][0] == '+' && arr[1][0] == '-')
 	{
-		str = s;
-		s = ft_strjoin("+", s, -1);
+		str = ft_strdup(s);
+		if (arr[1][0] == '+')
+			s = ft_strjoin("+", str, -1);
+		else if (arr[1][0] == '-')
+			s = ft_strjoin("-", str, -1);
 		free(str);
 	}
 	if (strlen_2d(arr) == 1 && arr[1] == NULL)
-		exit(exit_error_code(*env));
+		exit(exit_error_code(*env) + free_of_n(s, arr, NULL, 3));
 	exit_num = check_exit_status(arr, s, exit_num);
-	if (exit_num != -1)
+	if (exit_num != 1000)
 		exit(exit_num + free_of_n(s, arr, NULL, 3));
-	free(s);
 }
 
 int	exit_error_code(t_env *env)
@@ -55,7 +58,7 @@ int	exit_error_code(t_env *env)
 		tmp = tmp->next;
 	}
 	ft_printf(1, "exit\n");
-	return(ft_atoi(tmp->data));
+	return (ft_atoi(tmp->data));
 }
 
 int	check_exit_status(char **arr, char *s, int exit_num)
@@ -70,14 +73,29 @@ int	check_exit_status(char **arr, char *s, int exit_num)
 	else if (ft_strlen(s) > 19 || check_digit(arr[1]) == 1 || \
 		ft_strcmp(s, arr[1]) != 0)
 	{
-		ft_printf(2, "exit\n Minishell: exit: %s: numeric argument required\n", \
+		ft_printf(2, "exit\nMinishell: exit: %s: numeric argument required\n", \
 			arr[1]);
 		return (255);
 	}
 	else if (strlen_2d(arr) > 2 && check_digit(arr[1]) == 0)
 	{
-		ft_printf(2, "exit\n Minishell: exit: too many arguments\n");
-		return (-1);
+		ft_printf(2, "exit\nMinishell: exit: too many arguments\n");
+		free(s);
+		return (1000);
 	}
 	return (-1);
+}
+
+char	*check_zeroes(char *str)
+{
+	char *tmp;
+
+	if (str != NULL && (str[0] == '0' || \
+			((str[0] == '+' || str[0] == '-') && str[1] == '0')))
+	{
+		tmp = ft_strdup(str);
+		str = trim_zeroes(tmp);
+		free(tmp);
+	}
+	return (ft_strdup(str));
 }
